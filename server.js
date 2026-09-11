6045ece9283c7612073d94ae3e3a35bd1362445c
Nabeel Ahmad <nabeelahmadoff@gmail.com>
2026-09-12 00:07:38 +0530

    Learning Prisma With Postgre sql

---
+++ server.js
@@ -0,0 +1,12 @@
+import "dotenv/config"
+import express from "express"
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req,res) => {
+    return res.send("Hello world")
+})
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
